82833b19451f7df22aaaeed71cb0a3bb58658850
Dark-WizZ <mailwithakram@gmail.com>
2022-08-18 00:51:39 +0530

    Implement row-col design

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let Gameboard = function(){
-  let Gameboard = [];
+  let Gameboard = [[],[],[]];
   return {Gameboard};
 }()
 
@@ -33,18 +33,38 @@ let DisplayController = function(){
     layoutItems();
   }
   function plotMarks(){
-    let index = this.classList[1];
-    if (gameBoard.Gameboard[index]!==undefined) return;
-    gameBoard.Gameboard[index] = currentPlayer.mark;
+    let index = this.classList[1].split('-');
+    let row = index[0];
+    let col = index[1];
+    if (gameBoard.Gameboard[row][col]!==undefined) return;
+    gameBoard.Gameboard[row][col] = currentPlayer.mark;
     layoutItems();
+    check({row, col});
     togglePlayer();
   }
-  function layoutItems(){ 
-    for(let i=0; i<parts.length; i++){
-      parts[i].textContent = gameBoard.Gameboard[i];
+  function layoutItems(){
+    let i = 0;
+    for(let row=0; row<3; row++){
+      for(let col=0; col<3; col++){
+        parts[i].textContent = gameBoard.Gameboard[row][col];
+        i++;
+      }
     }
   }
   function togglePlayer(){
     currentPlayer = (currentPlayer==playerX)? playerO : playerX;
+  }
+  function check(pos){
+    checkRow(pos);
+  }
+  function checkRow(pos){
+    for(let i=0; i<2; i++){
+      let col = (i<2)? i : 0;
+      if(gameBoard.Gameboard[pos.row][col]==gameBoard.Gameboard[pos.row][++col]){
+        console.log(col)
+        return;
+      }
+    }
+    console.log('lose');
   }
 }()
