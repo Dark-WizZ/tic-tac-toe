21226525ea59b87218f4058a562239ae3f6c6bce
Dark-WizZ <mailwithakram@gmail.com>
2022-08-17 23:56:08 +0530

    Add interaction on layout

--- script.js
+++ script.js
@@ -1,3 +1,50 @@
-(function(){
-  
-})()
+let Gameboard = function(){
+  let Gameboard = [];
+  return {Gameboard};
+}()
+
+let Player = function(mark){
+  this.mark = mark;
+  return {mark};
+}
+
+let DisplayController = function(){
+
+  //dom reference
+  const parts = document.querySelectorAll('.part');
+
+  //bindEvents
+  parts.forEach(e => {
+    e.addEventListener('click', plotMarks)
+  })
+
+  //players
+  const playerX = Player('X');
+  const playerO = Player('O');
+  let currentPlayer = playerX;
+
+  const gameBoard = Gameboard;
+
+  //init
+  render();
+
+  //methods
+  function render(){
+    layoutItems();
+  }
+  function plotMarks(){
+    let index = this.classList[1];
+    if (gameBoard.Gameboard[index]!==undefined) return;
+    gameBoard.Gameboard[index] = currentPlayer.mark;
+    layoutItems();
+    togglePlayer();
+  }
+  function layoutItems(){ 
+    for(let i=0; i<parts.length; i++){
+      parts[i].textContent = gameBoard.Gameboard[i];
+    }
+  }
+  function togglePlayer(){
+    currentPlayer = (currentPlayer==playerX)? playerO : playerX;
+  }
+}()
