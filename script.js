8a206c5496cad9909008c8fd262fd29065f1be0d
Dark-WizZ <mailwithakram@gmail.com>
2022-08-18 15:33:32 +0530

    Add welcome screen

--- script.js
+++ script.js
@@ -13,15 +13,20 @@ let DisplayController = function(){
 
   //dom reference
   const parts = document.querySelectorAll('.part');
+  const playBtn = document.querySelector('.play');
+  const gameLayout = document.querySelector('.game-layout');
+  const welcomeLayout = document.querySelector('.welcome-layout');
+  const playerInfo = document.querySelector('.player-info');
 
   //bindEvents
   parts.forEach(e => {
     e.addEventListener('click', plotMarks)
   })
+  playBtn.addEventListener('click', playBtnClick);
 
   //players
-  const playerX = Player('Akram','X');
-  const playerO = Player('Lux','O');
+  let playerX = Player('Player1','X');
+  let playerO = Player('Player2','O');
   let currentPlayer = playerX;
   let turn = 0;
 
@@ -56,7 +61,10 @@ let DisplayController = function(){
     }
   }
   function togglePlayer(){
+    console.log(currentPlayer)
+    console.log(playerX);
     currentPlayer = (currentPlayer==playerX)? playerO : playerX;
+    playerInfo.textContent = `${currentPlayer.name}'s turn`;
   }
   function check(pos){
     checkRow(pos);
@@ -101,5 +109,15 @@ let DisplayController = function(){
   }
   function lose(){
     alert(currentPlayer.name + ' won!')
+  }
+  function playBtnClick(){
+    const player1IP = document.querySelector('#player1-name');
+    const player2IP = document.querySelector('#player2-name');
+    playerX = Player(player1IP.value, 'X');
+    playerO = Player(player2IP.value, 'O');
+    currentPlayer = playerX;
+    welcomeLayout.style.display = 'none';
+    gameLayout.style.display = 'grid';
+    playerInfo.textContent = `${playerX.name}'s turn`;
   }
 }()
